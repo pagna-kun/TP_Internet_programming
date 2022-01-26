aa78d11a285b84a4879a3802c47715aa645bd5cc
nha <pagna1111@gmail.com>
2022-01-26 20:34:23 +0700

    all done

--- ex2-login-form-with-cookie/script.js
+++ ex2-login-form-with-cookie/script.js
@@ -1,24 +1,50 @@
-localStorage.setItem("tp3Cookies",JSON.stringify({"email":"gic@gmail.com", "pwd":"Gic123"}));
-var acc = JSON.parse(localStorage.getItem("tp3Cookies"));
-console.log(acc.email);
-console.log(acc.pwd);
+/*static user acc*/
+var email = "gic@gmail.com";
+var password = "Gic123";
 
+/*function to get cookie value by name*/
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+}
+
+/*function login*/
 function login(){
+    /*set cookie*/
+    var date = new Date();
+    date.setTime(date.getTime()+(30*1000));
+    document.cookie="email=gic@gmail.com;expires="+date.toUTCString();
+    
+    /*check user acc*/
     let email = document.getElementById('email').value;
     let password = document.getElementById('password').value;
+    
     // console.log(email, password);
-    if(acc.email === email && acc.pwd === password){
-        document.getElementById("wrapper").innerHTML = "";
-        document.getElementById("wrapper").innerHTML = 
-            `<div class="content">
-                <h1>Welcome to Home Page</h1>
-                <p>You will be automatically logged out</p>
-                <p>Keep refreshing your page</p>
-                <button>Logout</button>
-            </div>`
+    if(this.email === email && this.password === password && document.cookie === "email=gic@gmail.com") {
+        location.href="./home.html" ;
     }
 }
+
+/*function logout button*/
 function logout(){
-    
+    location.href = "./index.html";
 }
 
+/*function for reload home page when cookie is expired*/
+function cookieExpired(){
+    var cookieValue = getCookie("email");
+    if(cookieValue != "gic@gmail.com"){
+        location.href = "./index.html";
+    }
+}
