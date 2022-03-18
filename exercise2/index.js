3619694c4b21285e8408e3e2888890bdeeafd10f
nha <pagna1111@gmail.com>
2022-03-18 14:35:40 +0700

    nth

--- exercise2/index.js
+++ exercise2/index.js
@@ -1,9 +1,13 @@
 var express = require('express')
 var bodyParser = require('body-parser')
+var cors = require('cors')
 
 var app = express()
 const port = 3001;
 
+app.use(cors({
+    origin: 'http://localhost:3000'
+}))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
